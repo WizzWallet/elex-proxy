85e8250501be3d8bb03307c88bc03f680cfe1dc4
iota9star <iota9star@foxmail.com>
2024-01-17 01:52:50 +0800

    🚀 Improve performance

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,11 @@ use std::sync::atomic::{AtomicU32, Ordering};
 use std::time::Duration;
 
 use axum::body::Body;
-use axum::error_handling::HandleErrorLayer;
 use axum::extract::{Path, Query};
 use axum::extract::Extension;
 use axum::extract::Json;
 use axum::http;
-use axum::http::header;
+use axum::http::{header, HeaderMap};
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::response::Response;
@@ -25,6 +24,7 @@ use dotenv::dotenv;
 use futures::{SinkExt, StreamExt};
 use http_body_util::Full;
 use once_cell::sync::Lazy;
+use rand::Rng;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Number, Value};
 use tokio::sync::{mpsc, Mutex, RwLock};
@@ -32,16 +32,18 @@ use tokio::sync::oneshot;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use tokio_tungstenite::connect_async;
 use tokio_tungstenite::tungstenite::Message;
-use tower::{BoxError, ServiceBuilder};
 use tower::limit::ConcurrencyLimitLayer;
-use tower_governor::errors::display_error;
 use tower_governor::governor::GovernorConfigBuilder;
 use tower_governor::GovernorLayer;
+use tower_governor::key_extractor::SmartIpKeyExtractor;
 use tower_http::catch_panic::CatchPanicLayer;
 use tower_http::cors::CorsLayer;
 use tower_http::trace::TraceLayer;
 use tracing::{error, info, warn};
 
+use crate::ip::maybe_ip_from_headers;
+
+mod ip;
 mod urn;
 
 #[derive(Serialize)]
@@ -101,11 +103,19 @@ impl R {
     }
 }
 
-static IP_LIMIT_PER_SECOND: LazyLock<u64> = LazyLock::new(|| {
-    env::var("IP_LIMIT_PER_SECOND")
-        .unwrap_or("1".to_string())
+static IP_LIMIT_PER_MILLS: LazyLock<u64> = LazyLock::new(|| {
+    let per_second:u64 = env::var("IP_LIMIT_PER_SECOND")
+        .unwrap_or("0".to_string())
         .parse()
-        .unwrap()
+        .unwrap();
+    if per_second > 0 {
+        per_second * 1000
+    } else {
+        env::var("IP_LIMIT_PER_MILLS")
+            .unwrap_or("10".to_string())
+            .parse()
+            .unwrap()
+    }
 });
 
 static IP_LIMIT_BURST_SIZE: LazyLock<u32> = LazyLock::new(|| {
@@ -124,6 +134,13 @@ static CONCURRENCY_LIMIT: LazyLock<usize> = LazyLock::new(|| {
 
 static ELECTRUMX_WSS: LazyLock<String> = LazyLock::new(|| {
     env::var("ELECTRUMX_WSS").unwrap_or("wss://electrumx.atomicals.xyz:50012".to_string())
+});
+
+static ELECTRUMX_WS_INSTANCE: LazyLock<u32> = LazyLock::new(|| {
+    env::var("ELECTRUMX_WS_INSTANCE")
+        .unwrap_or("1".to_string())
+        .parse()
+        .unwrap()
 });
 
 static PROXY_HOST: LazyLock<String> =
@@ -168,38 +185,52 @@ impl IntoResponse for R {
 }
 
 async fn handle_get(
-    Extension(callbacks): Extension<Callbacks>,
-    Extension(ws_tx): Extension<mpsc::UnboundedSender<JsonRpcRequest>>,
+    Extension(callbacks): Extension<Vec<(mpsc::UnboundedSender<JsonRpcRequest>, Callbacks)>>,
+    headers: HeaderMap,
     Path(method): Path<String>,
     Query(query): Query<Value>,
 ) -> Result<R, AppError> {
+    let item = random_callback(&callbacks);
+    let sender = item.0.clone();
+    let calls = item.1.clone();
     let r = match query.get("params") {
-        None => handle_request(callbacks, ws_tx, method, vec![]).await,
+        None => handle_request(sender, calls, headers, method, vec![]).await,
         Some(v) => {
             let x = v
                 .as_str()
                 .map(|s| if s.is_empty() { "[]" } else { s })
                 .unwrap();
             let params = serde_json::from_str(x).unwrap();
-            handle_request(callbacks, ws_tx, method, params).await
+            handle_request(sender, calls, headers, method, params).await
         }
     };
     Ok(r)
 }
 
+fn random_callback(
+    callbacks: &[(mpsc::UnboundedSender<JsonRpcRequest>, Callbacks)],
+) -> &(mpsc::UnboundedSender<JsonRpcRequest>, Callbacks) {
+    let mut rng = rand::thread_rng();
+    let index = rng.gen_range(0..callbacks.len());
+    &callbacks[index]
+}
+
 async fn handle_post(
-    Extension(callbacks): Extension<Callbacks>,
-    Extension(ws_tx): Extension<mpsc::UnboundedSender<JsonRpcRequest>>,
+    Extension(callbacks): Extension<Vec<(mpsc::UnboundedSender<JsonRpcRequest>, Callbacks)>>,
+    headers: HeaderMap,
     Path(method): Path<String>,
     body: Option<Json<Value>>,
 ) -> Result<R, AppError> {
+    let item = random_callback(&callbacks);
+    let sender = item.0.clone();
+    let calls = item.1.clone();
     let r = match body {
-        None => handle_request(callbacks, ws_tx, method, vec![]).await,
+        None => handle_request(sender, calls, headers, method, vec![]).await,
         Some(v) => match v.0.get("params") {
-            None => handle_request(callbacks, ws_tx, method, vec![]).await,
+            None => handle_request(sender, calls, headers, method, vec![]).await,
             Some(v) => {
                 let x = v.as_array().unwrap();
-                handle_request(callbacks, ws_tx, method, x.clone()).await
+                handle_request(sender, calls, headers, method, x.clone()).await
             }
         },
     };
@@ -207,13 +238,18 @@ async fn handle_post(
 }
 
 async fn handle_request(
-    callbacks: Callbacks,
     ws_tx: mpsc::UnboundedSender<JsonRpcRequest>,
+    callbacks: Callbacks,
+    headers: HeaderMap,
     method: String,
     params: Vec<Value>,
 ) -> R {
     let id = get_next_id();
-    info!("=> id: {}, method: {}, params: {:?}", &id, &method, &params);
+    let addr = maybe_ip_from_headers(&headers);
+    info!(
+        "{} => {}, {}({:?})",
+        &addr, &id, &method, &params
+    );
     let (response_tx, response_rx) = oneshot::channel();
     {
         callbacks.write().await.insert(id, response_tx);
@@ -239,8 +275,8 @@ async fn handle_request(
         }
         Ok(Err(_)) | Err(_) => {
             warn!(
-                "<= id: {}, no response received after {} seconds",
-                &id, *RESPONSE_TIMEOUT
+                "{} <= {}, No response received within {} seconds",
+                &addr, &id, *RESPONSE_TIMEOUT
             );
             {
                 callbacks.write().await.remove(&id);
@@ -251,31 +287,33 @@ async fn handle_request(
 }
 
 async fn handle_health(
-    Extension(callbacks): Extension<Callbacks>,
-    Extension(ws_tx): Extension<mpsc::UnboundedSender<JsonRpcRequest>>,
+    Extension(callbacks): Extension<Vec<(mpsc::UnboundedSender<JsonRpcRequest>, Callbacks)>>,
+    headers: HeaderMap,
 ) -> impl IntoResponse {
     let id = get_next_id();
-    info!("=> id: {}, check health", &id);
+    let item = random_callback(&callbacks);
+    let addr = maybe_ip_from_headers(&headers);
+    info!("{} => {}, Detecting server health", &addr, &id);
 
     let (response_tx, response_rx) = oneshot::channel();
     {
-        callbacks.write().await.insert(id, response_tx);
+        item.1.write().await.insert(id, response_tx);
     }
     let request = JsonRpcRequest {
         id,
         method: "blockchain.atomicals.get_global".into(),
         params: vec![],
     };
-    ws_tx.send(request).unwrap();
+    item.0.send(request).unwrap();
     match tokio::time::timeout(Duration::from_secs(5), response_rx).await {
         Ok(Ok(rep)) => R::health(rep.result.is_some()),
         Ok(Err(_)) | Err(_) => {
             warn!(
-                "<= id: {}, check health timeout, no response received after 5 seconds",
-                &id
+                "{} <= {}, Check server health timeout, no response received within 5 seconds",
+                &addr, &id
             );
             {
-                callbacks.write().await.remove(&id);
+                item.1.write().await.remove(&id);
             }
             R::health(false)
         }
@@ -322,16 +360,21 @@ fn handle_panic(err: Box<dyn Any + Send + 'static>) -> http::Response<Full<Bytes
 async fn main() {
     dotenv().ok();
     tracing_subscriber::fmt::init();
-    let (ws_tx, ws_rx) = mpsc::unbounded_channel::<JsonRpcRequest>();
-    let callbacks: Callbacks = Arc::new(RwLock::new(HashMap::new()));
-    let ws_rx_stream = Arc::new(Mutex::new(UnboundedReceiverStream::new(ws_rx)));
     let governor_conf = Box::new(
         GovernorConfigBuilder::default()
-            .per_second(*IP_LIMIT_PER_SECOND)
+            .per_millisecond(*IP_LIMIT_PER_MILLS)
             .burst_size(*IP_LIMIT_BURST_SIZE)
+            .key_extractor(SmartIpKeyExtractor)
+            .use_headers()
             .finish()
             .unwrap(),
     );
+    let mut calls = vec![];
+    for i in 0..*ELECTRUMX_WS_INSTANCE {
+        let (ws_tx, callbacks, ws_rx_stream) = new_callbacks();
+        calls.push((ws_tx, callbacks.clone()));
+        try_new_client(i, callbacks, ws_rx_stream);
+    }
     let app = Router::new()
         .fallback(|uri: http::Uri| async move {
             let body = R::error(-1, format!("No route: {}", &uri));
@@ -346,81 +389,14 @@ async fn main() {
         .route("/proxy", get(handle_proxy).post(handle_proxy))
         .route("/proxy/health", get(handle_health).post(handle_health))
         .route("/proxy/:method", get(handle_get).post(handle_post))
-        .layer(
-            ServiceBuilder::new()
-                .layer(HandleErrorLayer::new(|e: BoxError| async move {
-                    display_error(e)
-                }))
-                .layer(CatchPanicLayer::custom(handle_panic))
-                .layer(TraceLayer::new_for_http())
-                .layer(GovernorLayer {
-                    config: Box::leak(governor_conf),
-                }),
-        )
+        .layer(GovernorLayer {
+            config: Box::leak(governor_conf),
+        })
         .layer(ConcurrencyLimitLayer::new(*CONCURRENCY_LIMIT))
+        .layer(CatchPanicLayer::custom(handle_panic))
+        .layer(TraceLayer::new_for_http())
         .layer(CorsLayer::permissive())
-        .layer(Extension(callbacks.clone()))
-        .layer(Extension(ws_tx.clone()));
-
-    tokio::spawn(async move {
-        let list = ELECTRUMX_WSS.split(',').collect::<Vec<&str>>();
-        info!("ElectrumX WSS: {:?}", &list);
-        let mut index = 0;
-        loop {
-            let wss = list.get(index).unwrap();
-            info!("Try to connect to ElectrumX: {}", &wss);
-            match connect_async(*wss).await {
-                Ok((ws, _)) => {
-                    info!("Connected to ElectrumX: {}", &wss);
-                    let (mut write, mut read) = ws.split();
-                    let ws_rx_stream = Arc::clone(&ws_rx_stream);
-                    let send_handle = tokio::spawn(async move {
-                        let mut guard = ws_rx_stream.lock().await;
-                        while let Some(message) = guard.next().await {
-                            let request_text = serde_json::to_string(&message).unwrap();
-                            if let Err(e) = write.send(Message::Text(request_text)).await {
-                                error!("Failed to send message to ElectrumX: {:?}", e);
-                                break;
-                            }
-                        }
-                    });
-                    while let Some(Ok(msg)) = read.next().await {
-                        if msg.is_text() {
-                            if let Ok(text) = msg.to_text() {
-                                if let Ok(resp) = serde_json::from_str::<JsonRpcResponse>(text) {
-                                    if let Some(callback) = callbacks.write().await.remove(&resp.id)
-                                    {
-                                        info!("<= id: {}, processed", &resp.id);
-                                        let _ = callback.send(resp);
-                                    } else {
-                                        warn!("<= id: {}, not processed", &resp.id);
-                                    }
-                                } else {
-                                    error!("Failed to parse ws response: {}", text);
-                                }
-                            }
-                        } else if msg.is_close() {
-                            warn!("Connection closed: {}", &wss);
-                            // Close the send handle to stop the send task.
-                            if !send_handle.is_finished() {
-                                send_handle.abort();
-                            }
-                            break;
-                        }
-                    }
-                }
-                Err(e) => {
-                    error!("Failed to connect to ElectrumX: {:?}", e);
-                    tokio::time::sleep(Duration::from_secs(3)).await;
-                }
-            }
-            if index >= list.len() - 1 {
-                index = 0;
-            } else {
-                index += 1;
-            }
-        }
-    });
+        .layer(Extension(calls.clone()));
     let listener = tokio::net::TcpListener::bind((*PROXY_HOST).clone())
         .await
         .unwrap();
@@ -431,4 +407,81 @@ async fn main() {
     )
     .await
     .unwrap();
+}
+
+fn new_callbacks() -> (
+    mpsc::UnboundedSender<JsonRpcRequest>,
+    Callbacks,
+    Arc<Mutex<UnboundedReceiverStream<JsonRpcRequest>>>,
+) {
+    let (ws_tx, ws_rx) = mpsc::unbounded_channel::<JsonRpcRequest>();
+    let callbacks: Callbacks = Arc::new(RwLock::new(HashMap::new()));
+    let ws_rx_stream = Arc::new(Mutex::new(UnboundedReceiverStream::new(ws_rx)));
+    (ws_tx, callbacks, ws_rx_stream)
+}
+
+fn try_new_client(
+    ins: u32,
+    callbacks: Callbacks,
+    ws_rx_stream: Arc<Mutex<UnboundedReceiverStream<JsonRpcRequest>>>,
+) {
+    tokio::spawn(async move {
+        let list = ELECTRUMX_WSS.split(',').collect::<Vec<&str>>();
+        info!("WS-{} ElectrumX WSS: {:?}", ins, &list);
+        let mut index = 0;
+        loop {
+            let wss = list.get(index).unwrap();
+            info!("WS-{} Try to connect to ElectrumX: {}", ins, &wss);
+            match connect_async(*wss).await {
+                Ok((ws, _)) => {
+                    info!("WS-{} Connected to ElectrumX: {}", ins, &wss);
+                    let (mut write, mut read) = ws.split();
+                    let ws_rx_stream = Arc::clone(&ws_rx_stream);
+                    let send_handle = tokio::spawn(async move {
+                        let mut guard = ws_rx_stream.lock().await;
+                        while let Some(message) = guard.next().await {
+                            let request_text = serde_json::to_string(&message).unwrap();
+                            if let Err(e) = write.send(Message::Text(request_text)).await {
+                                error!("WS-{} Failed to send message to ElectrumX: {:?}", ins, e);
+                                break;
+                            }
+                        }
+                    });
+                    while let Some(Ok(msg)) = read.next().await {
+                        if msg.is_text() {
+                            if let Ok(text) = msg.to_text() {
+                                if let Ok(resp) = serde_json::from_str::<JsonRpcResponse>(text) {
+                                    if let Some(callback) = callbacks.write().await.remove(&resp.id)
+                                    {
+                                        info!("WS-{} <= {}, Request matched", ins, &resp.id);
+                                        let _ = callback.send(resp);
+                                    } else {
+                                        warn!("WS-{} <= {}, No matching request found", ins, &resp.id);
+                                    }
+                                } else {
+                                    error!("WS-{} Failed to parse ws response: {}", ins, text);
+                                }
+                            }
+                        } else if msg.is_close() {
+                            warn!("WS-{} Connection closed: {}", ins, &wss);
+                            // Close the send handle to stop the send task.
+                            if !send_handle.is_finished() {
+                                send_handle.abort();
+                            }
+                            break;
+                        }
+                    }
+                }
+                Err(e) => {
+                    error!("WS-{} Failed to connect to ElectrumX: {:?}", ins, e);
+                    tokio::time::sleep(Duration::from_secs(3)).await;
+                }
+            }
+            if index >= list.len() - 1 {
+                index = 0;
+            } else {
+                index += 1;
+            }
+        }
+    });
 }
--- src/urn.rs
+++ src/urn.rs
@@ -1,92 +1,93 @@
-use anyhow::{anyhow, Result};
-use axum::Extension;
-use axum::extract::Path;
-use futures::future::ok;
-use regex::Regex;
-use serde_json::Value;
-use tokio::sync::mpsc;
-use tracing::info;
-
-use crate::{AppError, Callbacks, handle_request, JsonRpcRequest, R};
-
-async fn handle_urn(
-    Extension(callbacks): Extension<Callbacks>,
-    Extension(ws_tx): Extension<mpsc::UnboundedSender<JsonRpcRequest>>,
-    Path(urn): Path<String>,
-) -> std::result::Result<R, AppError> {
-    let result = decode_urn(&urn).unwrap();
-    info!("URN info: {:?}", result);
-    match result.urn_type {
-        UrnType::AtomicalId => {
-            Ok(R::ok(Value::Null))
-        }
-        UrnType::Realm => {
-            let r = handle_request(callbacks, ws_tx, "blockchain.atomicals.get_by_realm".into(), vec![Value::String(result.identifier)]).await;
-            if let Some(v) = r.response {
-                let atomical_id = v.as_object().unwrap().get("result").unwrap().as_object().unwrap().get("atomical_id").unwrap().as_str();
-                // if let Some(v) = atomical_id{
-                //
-                // }else {
-                //     Ok(R::error(-1,format!("realm not found: {}", &result.identifier)))
-                // }
-                Ok(R::ok(Value::Null))
-            }else {
-                Ok(r)
-            }
-        }
-        UrnType::Container => {
-            Ok(R::ok(Value::Null))
-        }
-        UrnType::Dat => {
-            Ok(R::ok(Value::Null))
-        }
-        UrnType::Arc => {
-            Ok(R::ok(Value::Null))
-        }
-    }
-}
-
-#[derive(Debug)]
-pub enum UrnType {
-    AtomicalId = 0,
-    Realm = 1,
-    Container = 2,
-    Dat = 3,
-    Arc = 4,
-}
-
-#[derive(Debug)]
-pub struct UrnInfo {
-    pub urn_type: UrnType,
-    pub identifier: String,
-    pub path_type: Option<String>,
-    pub path: Option<String>,
-}
-
-pub fn decode_urn(urn: &str) -> Result<UrnInfo> {
-    if let Some(captures) = Regex::new(r"atom:btc:dat:([0-9a-f]{64}i\d+)(([/$])?.*)")?.captures(urn) {
-        return process_match(UrnType::Dat, captures);
-    }
-    if let Some(captures) = Regex::new(r"atom:btc:id:([0-9a-f]{64}i\d+)(([/$])?.*)")?.captures(urn) {
-        return process_match(UrnType::AtomicalId, captures);
-    }
-    if let Some(captures) = Regex::new(r"atom:btc:realm:(.+)(([/$])?.*)")?.captures(urn) {
-        return process_match(UrnType::Realm, captures);
-    }
-    if let Some(captures) = Regex::new(r"atom:btc:container:(.+)(([/$])?.*)")?.captures(urn) {
-        return process_match(UrnType::Container, captures);
-    }
-    if let Some(captures) = Regex::new(r"atom:btc:arc:(.+)(([/$])?.*)")?.captures(urn) {
-        return process_match(UrnType::Arc, captures);
-    }
-    Err(anyhow!("Invalid URN"))
-}
-
-fn process_match(urn_type: UrnType, captures: regex::Captures) -> Result<UrnInfo> {
-    Ok(UrnInfo {
-        urn_type,
-        identifier: captures[1].to_string(),
-        path_type: Some(captures.get(3).map_or("", |m| m.as_str()).to_string()),
-        path: captures.get(2).map(|m| m.as_str().to_string()),
-    })
-}
+// use anyhow::{anyhow, Result};
+// use axum::Extension;
+// use axum::extract::Path;
+// use headers::HeaderMap;
+// use regex::Regex;
+// use serde_json::Value;
+// use tokio::sync::mpsc;
+// use tracing::info;
+//
+// use crate::{AppError, Callbacks, handle_request, JsonRpcRequest, R};
+//
+// async fn handle_urn(
+//     Extension(callbacks): Extension<Callbacks>,
+//     Extension(ws_tx): Extension<mpsc::UnboundedSender<JsonRpcRequest>>,
+//     headers:HeaderMap,
+//     Path(urn): Path<String>,
+// ) -> std::result::Result<R, AppError> {
+//     let result = decode_urn(&urn).unwrap();
+//     info!("URN info: {:?}", result);
+//     match result.urn_type {
+//         UrnType::AtomicalId => {
+//             Ok(R::ok(Value::Null))
+//         }
+//         UrnType::Realm => {
+//             let r = handle_request(ws_tx, callbacks, headers, "blockchain.atomicals.get_by_realm".into(), vec![Value::String(result.identifier)]).await;
+//             if let Some(v) = r.response {
+//                 let atomical_id = v.as_object().unwrap().get("result").unwrap().as_object().unwrap().get("atomical_id").unwrap().as_str();
+//                 // if let Some(v) = atomical_id{
+//                 //
+//                 // }else {
+//                 //     Ok(R::error(-1,format!("realm not found: {}", &result.identifier)))
+//                 // }
+//                 Ok(R::ok(Value::Null))
+//             }else {
+//                 Ok(r)
+//             }
+//         }
+//         UrnType::Container => {
+//             Ok(R::ok(Value::Null))
+//         }
+//         UrnType::Dat => {
+//             Ok(R::ok(Value::Null))
+//         }
+//         UrnType::Arc => {
+//             Ok(R::ok(Value::Null))
+//         }
+//     }
+// }
+//
+// #[derive(Debug)]
+// pub enum UrnType {
+//     AtomicalId = 0,
+//     Realm = 1,
+//     Container = 2,
+//     Dat = 3,
+//     Arc = 4,
+// }
+//
+// #[derive(Debug)]
+// pub struct UrnInfo {
+//     pub urn_type: UrnType,
+//     pub identifier: String,
+//     pub path_type: Option<String>,
+//     pub path: Option<String>,
+// }
+//
+// pub fn decode_urn(urn: &str) -> Result<UrnInfo> {
+//     if let Some(captures) = Regex::new(r"atom:btc:dat:([0-9a-f]{64}i\d+)(([/$])?.*)")?.captures(urn) {
+//         return process_match(UrnType::Dat, captures);
+//     }
+//     if let Some(captures) = Regex::new(r"atom:btc:id:([0-9a-f]{64}i\d+)(([/$])?.*)")?.captures(urn) {
+//         return process_match(UrnType::AtomicalId, captures);
+//     }
+//     if let Some(captures) = Regex::new(r"atom:btc:realm:(.+)(([/$])?.*)")?.captures(urn) {
+//         return process_match(UrnType::Realm, captures);
+//     }
+//     if let Some(captures) = Regex::new(r"atom:btc:container:(.+)(([/$])?.*)")?.captures(urn) {
+//         return process_match(UrnType::Container, captures);
+//     }
+//     if let Some(captures) = Regex::new(r"atom:btc:arc:(.+)(([/$])?.*)")?.captures(urn) {
+//         return process_match(UrnType::Arc, captures);
+//     }
+//     Err(anyhow!("Invalid URN"))
+// }
+//
+// fn process_match(urn_type: UrnType, captures: regex::Captures) -> Result<UrnInfo> {
+//     Ok(UrnInfo {
+//         urn_type,
+//         identifier: captures[1].to_string(),
+//         path_type: Some(captures.get(3).map_or("", |m| m.as_str()).to_string()),
+//         path: captures.get(2).map(|m| m.as_str().to_string()),
+//     })
+// }
